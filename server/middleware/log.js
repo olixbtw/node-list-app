d99f86bd014d199bedfb14eef32bf9b458ec5daa
Oleksandr <oleksandr_revchuk@epam.com>
2019-10-04 17:50:58 +0300

    added colors to logging

--- server/middleware/log.js
+++ server/middleware/log.js
@@ -1,4 +1,34 @@
+var colors = require('colors');
+
+colors.setTheme({
+  get: 'green',
+  post: 'blue',
+  put: 'yellow',
+  delete: 'red',
+  default: 'white'
+});
+
+
 module.exports = (req, response, next) => {
-  console.log(`${req.method} ${req.url}`);
+
+  switch (req.method) {
+    case "GET":
+      console.log(`${req.method}`.get + ` ${req.url}`.default);
+      break;
+    case "POST":
+      console.log(`${req.method}`.post + ` ${req.url}`.default);
+      break;
+    case "PUT":
+      console.log(`${req.method}`.put + ` ${req.url}`.default);
+      break;
+    case "DELETE":
+      console.log(`${req.method}`.delete + ` ${req.url}`.default);
+      break;
+
+    default:
+      console.log(`${req.method} ${req.url}`.default);
+      break;
+  }
+
   next();
 };
